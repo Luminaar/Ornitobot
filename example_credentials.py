76615a026876a3aa6cfbf191b1501bb07970ade2
Luminaar <kovykmax@gmail.com>
2015-04-28 12:10:07 +0200

    Added instructions

---
+++ example_credentials.py
@@ -0,0 +1,7 @@
+# Put in your credentials generated on dev.twitter.com and
+# rename the file to credentials.py
+
+key = 'consumer_key'
+secret = 'consumer_secret'
+token_key = 'access_token_key'
+token_secret = 'access_token_secret' 
